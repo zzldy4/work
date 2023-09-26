17f3c583e6d75485f90ea9a6493ddf463406eb3e
Allen <1615227867@qq.com>
2023-09-26 19:24:25 +0800

    9月26日

---
+++ server/src/main/java/cn/ccsu/config/WebMvcConfiguration.java
@@ -0,0 +1,10 @@
+package cn.ccsu.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+@Slf4j
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+}
